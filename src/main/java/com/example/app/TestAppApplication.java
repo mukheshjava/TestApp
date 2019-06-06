161401e4d6c7e106b1203a8a98928ca1498ed27c
sony <sony@Mukhesh-Vaio>
2019-06-06 13:29:15 +0530

    1st commit

---
+++ src/main/java/com/example/app/TestAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppApplication.class, args);
+	System.out.println("Done");
+	}
+
+}
